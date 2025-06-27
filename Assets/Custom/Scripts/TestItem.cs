c423488824e2c56c9c90d77e0e4cb9255f6c3e1a
Gun <thanapat2027@gmail.com>
2025-06-28 00:25:59 +0700

    Added item interaction system with camerawork

--- Assets/Custom/Scripts/TestItem.cs
+++ Assets/Custom/Scripts/TestItem.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 using Unity.Cinemachine;
 public class TestItem : MonoBehaviour
 {
-    public CinemachineCamera camera;
+    public CinemachineCamera viewingCamera;
+    public CinemachineCamera interactingCamera;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
