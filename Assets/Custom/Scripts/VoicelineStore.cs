6b7e53941f5c36c5e6add15936226355adcbfd83
Gun <thanapat2027@gmail.com>
2025-09-30 10:13:19 +0700

    Unfinished VoicelineManager changes

--- Assets/Custom/Scripts/VoicelineStore.cs
+++ Assets/Custom/Scripts/VoicelineStore.cs
@@ -4,9 +4,12 @@ using System.Collections.Generic;
 using System;
 using Sirenix.OdinInspector;
 
+public enum Character { FASAI, NATE, NUENG, SOMCHAI }
+
 [CreateAssetMenu(fileName = "VoicelineStore", menuName = "Project Rahu/VoicelineStore")]
 public class VoicelineStore : SerializedScriptableObject
 {
     public List<AudioDataStore.DialogueLine> cutsceneVoicelines = new List<AudioDataStore.DialogueLine>();
-    public Dictionary<String,AudioDataStore.DialogueLine> eventBasedVoicelines = new Dictionary<string, AudioDataStore.DialogueLine>();
+    public Dictionary<Character, List<AudioDataStore.DialogueLine>> eventBasedVoicelines = new Dictionary<Character, List<AudioDataStore.DialogueLine>>();
+    public Dictionary<string, List<AudioDataStore.DialogueLine>> randomVoicelinePools = new Dictionary<string, List<AudioDataStore.DialogueLine>>();
 }
