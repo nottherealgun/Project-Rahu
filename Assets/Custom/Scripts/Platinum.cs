87649708559d5f272c83dbb822a10b2c3628044a
Gun <thanapat2027@gmail.com>
2025-10-14 10:57:00 +0700

    Added puzzle interaction closing on platinum pickup

--- Assets/Custom/Scripts/Platinum.cs
+++ Assets/Custom/Scripts/Platinum.cs
@@ -4,6 +4,7 @@ public class Platinum : SerializedMonoBehaviour
 {
     public void EndGame()
     {
+        UIManager.Instance.CloseInteractionHUD();
         PersistentDataManager.Instance.puzzles[PuzzleType.Platinum] = true;
         GameManager.Instance.OnPuzzleComplete();
     }
